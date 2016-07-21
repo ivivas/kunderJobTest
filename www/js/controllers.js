ffcb36073d7bf92100a26e96578a5f6d8821a9a6
ievivas <ivanvivas@gmail.com>
2016-07-20 22:51:00 -0400

    Add request fuction. List comics on tab-comic

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,7 +2,7 @@ angular.module('starter.controllers', [])
 
 .controller('SearchCtrl', function($scope) {})
 
-.controller('ComicsCtrl', function($scope, Comics) {
+.controller('ComicsCtrl', function($scope, $http) {
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
@@ -11,9 +11,31 @@ angular.module('starter.controllers', [])
   //$scope.$on('$ionicView.enter', function(e) {
   //});
 
-  $scope.comics = Comics.all();
+  // Variables for api request
+  var apikey = "f528484f82831a33b68df91a847bd45a";
+  var hash = "780f995c4717391fae2df679e3abaccd";
+  var ts = "1469041077";
+  var baseUrl = "http://gateway.marvel.com/v1/public/comics?format=comic&formatType=comic&&dateRange=2005-01-01%2C2010-01-01&orderBy=title&limit=50";
+  var requestUrl = baseUrl + "&ts=" + ts + "&apikey=" + apikey + "&hash=" + hash;
+  $http.get(requestUrl)
+  .then(function(response) {
+      var comics = [];
+      var comicObject = response.data;
+      for (var i = 0; i < 50; i++) {
+        comics.push({id: comicObject.data.results[i].id,
+                     title: comicObject.data.results[i].title,
+                     thumbnail: comicObject.data.results[i].thumbnail.path + "/standard_large.jpg",
+                     format: comicObject.data.results[i].format,
+                     url: comicObject.data.results[i].urls[0].url,
+                     series: comicObject.data.results[i].series.name,
+                     onsaleDate: comicObject.data.results[i].dates[0].date,
+        });
+      }
+      console.log(comics);
+      $scope.comics = comics;
+  });
 })
 
 .controller('ComicDetailCtrl', function($scope, $stateParams, Comics) {
-  $scope.comic = Comics.get($stateParams.comicId);
+  //$scope.comic = Comics.get($stateParams.comicId);
 });
--- www/js/services.js
+++ www/js/services.js
@@ -1,31 +1,6 @@
 angular.module('starter.services', [])
 
 .factory('Comics', function() {
-  // Variables for api request
-  var apikey = "f528484f82831a33b68df91a847bd45a";
-  var hash = "780f995c4717391fae2df679e3abaccd";
-  var ts = "1469041077";
-  var baseUrl = "http://gateway.marvel.com/v1/public/comics?format=comic&formatType=comic&orderBy=onsaleDate&limit=30";
-  var requestUrl = baseUrl + "&ts=" + ts + "&apikey=" + apikey + "&hash=" + hash
-  var xmlhttp = new XMLHttpRequest();
-  var comics = null;
-  //console.log();
-
-  xmlhttp.onreadystatechange = function() {
-    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-        var comicObject = JSON.parse(xmlhttp.responseText);
-        console.log(typeof comicObject);
-        extractValues(comicObject);
-    }
-  };
-  xmlhttp.open("GET", requestUrl, true);
-  xmlhttp.send();
-
-  //Function to extract the most significant values from comicObject
-  function extractValues(obj) {
-
-  }
-
   // Might use a resource here that returns a JSON array
   // Some fake testing data
   /* var comics = [{
@@ -55,17 +30,18 @@ angular.module('starter.services', [])
     face: 'img/mike.png'
   }];
  */
-  return {
+  /* return {
     all: function() {
       return comics;
     },
     get: function(comicId) {
-      for (var i = 0; i < comics.length; i++) {
-        if (comics[i].id === parseInt(comicId)) {
-          return comics[i];
+      for (var j = 0; j < comics.length; j++) {
+        if (comics[j].id === parseInt(comicId)) {
+          return comics[j];
         }
       }
       return null;
     }
-  };
+  }; */
+  return null;
 });
